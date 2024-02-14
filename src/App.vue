55292de63e147bf23dac2a370df2d6f6b8578160
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2024-02-14 23:43:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+let scene, camera, renderer, controls, cube;
+</script>
+
+<style lang="scss" scoped>
+
+</style>
